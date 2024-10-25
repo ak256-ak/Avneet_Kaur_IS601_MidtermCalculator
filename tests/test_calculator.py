8ce37b2c68c65d9bf7496e7e0ca1115922a051bc
ak256 <ak256@njit.edu>
2024-10-25 17:23:26 -0400

    added this Test calculator.py

---
+++ tests/test_calculator.py
@@ -0,0 +1,42 @@
+import pytest
+from calculator.calculator import Calculator
+
+@pytest.fixture
+def calculator():
+    return Calculator()
+
+def test_add(calculator):
+    result = calculator.plugins['add'].execute(3, 2)
+    assert result == 5
+
+def test_subtract(calculator):
+    result = calculator.plugins['subtract'].execute(5, 3)
+    assert result == 2
+
+def test_multiply(calculator):
+    result = calculator.plugins['multiply'].execute(4, 3)
+    assert result == 12
+
+def test_divide(calculator):
+    result = calculator.plugins['divide'].execute(10, 2)
+    assert result == 5
+
+def test_divide_by_zero(calculator):
+    with pytest.raises(ValueError, match="Cannot divide by zero."):
+        calculator.plugins['divide'].execute(10, 0)
+
+def test_square(calculator):
+    result = calculator.plugins['square'].execute(4)
+    assert result == 16
+
+def test_exponent(calculator):
+    result = calculator.plugins['exponent'].execute(2, 3)
+    assert result == 8
+
+def test_squareroot(calculator):
+    result = calculator.plugins['squareroot'].execute(9)
+    assert result == 3
+
+def test_cube(calculator):
+    result = calculator.plugins['cube'].execute(3)
+    assert result == 27
